009946e15422969c219c4c2ee2abdbde0ca939ba
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-01-23 21:54:03 +0530

    textcard component

---
+++ src/components/TextCard/TextCard.jsx
@@ -0,0 +1,38 @@
+import s from "./style.module.css";
+import { Trash } from "react-bootstrap-icons";
+import { useState } from "react";
+
+export function TextCard({ title, content, subtitle, onClick, onClickTrash }) {
+  const [isCardHovered, setIsCardHovered] = useState(false);
+  const [isTrashHovered, setTrashHovered] = useState(false);
+
+  // e.stopPropagation to prevent event bubbling
+  function onClickTrash_(e) {
+    onClickTrash();
+    e.stopPropagation();
+  }
+  return (
+    <div
+      onClick={onClick}
+      className={`card ${s.container} `}
+      style={{ borderColor: isCardHovered ? "#daa520" : "transparent" }}
+      onMouseEnter={() => setIsCardHovered(true)}
+      onMouseLeave={() => setIsCardHovered(false)}
+    >
+      <div className="card-body">
+        <div>
+          <h5 className={s.title_row}>{title}</h5>
+          <Trash
+            onClick={onClickTrash_}
+            size={20}
+            style={{ color: isTrashHovered ? "#FF7373" : "#b8b8b8" }}
+            onMouseEnter={() => setTrashHovered(true)}
+            onMouseLeave={() => setTrashHovered(false)}
+          />
+        </div>
+        <h6 className={`card-subtitle mb-2 text-muted`}>{subtitle}</h6>
+        <p className={`card-text ${s.text_content}`}>{content}</p>
+      </div>
+    </div>
+  );
+}
