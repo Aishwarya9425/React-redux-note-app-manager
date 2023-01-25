7507220c1ecb84afdfde8e8ecf9538449a110baf
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-01-25 20:31:11 +0530

    create new note

--- src/components/NoteForm/NoteForm.jsx
+++ src/components/NoteForm/NoteForm.jsx
@@ -1,35 +1,70 @@
 import { ButtonPrimary } from "components/ButtonPrimary/ButtonPrimary";
 import { PencilFill, TrashFill } from "react-bootstrap-icons";
 import s from "./style.module.css";
+import { useState } from "react";
 export function NoteForm({ title, onClickEdit, onClickDelete, onSubmit }) {
+  //one state for all form values
+
+  const [formValues, setFormValues] = useState({
+    title: "",
+    content: "",
+  });
+
+  const updateFormValues = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    //this will replace prev values , so need to use previous form values
+    setFormValues({ ...formValues, [name]: value });
+  };
+
   //small components
+
   const actionIcons = (
     <>
       <div className="col-1">
-        <PencilFill className={s.icon} />
+        {onClickEdit && <PencilFill className={s.icon} />}
       </div>
       <div className="col-1">
-        <TrashFill className={s.icon} />
+        {onClickDelete && <TrashFill className={s.icon} />}
       </div>
     </>
   );
+
+  //update formValues when the value changes
   const titleInput = (
     <>
       <label className="form-label"> Title </label>
-      <input type="text" name="title" className="form-control" />
+      <input
+        onChange={updateFormValues}
+        type="text"
+        name="title"
+        className="form-control"
+      />
     </>
   );
 
   const contentInput = (
     <>
       <label className="form-label"> Content </label>
-      <textarea type="text" name="content" className="form-control" rows="5" />
+      <textarea
+        onChange={updateFormValues}
+        type="text"
+        name="content"
+        className="form-control"
+        rows="5"
+      />
     </>
   );
 
   const submitBtn = (
     <div className={s.submit_btn}>
-      <ButtonPrimary>Submit</ButtonPrimary>
+      <ButtonPrimary
+        onClick={() => {
+          onSubmit(formValues);
+        }}
+      >
+        Submit
+      </ButtonPrimary>
     </div>
   );
 
@@ -44,7 +79,7 @@ export function NoteForm({ title, onClickEdit, onClickDelete, onSubmit }) {
 
       <div className={`mb-3 ${s.title_input_container}`}>{titleInput}</div>
       <div className="mb-3">{contentInput}</div>
-      {submitBtn}
+      {onSubmit && submitBtn}
     </div>
   );
 }
--- src/pages/NoteCreate/NoteCreate.jsx
+++ src/pages/NoteCreate/NoteCreate.jsx
@@ -1,8 +1,30 @@
+import { NoteAPI } from "api/note-api";
 import { NoteForm } from "components/NoteForm/NoteForm";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { addNote } from "store/notes/notes-slice";
+
 export function NoteCreate(props) {
+  const dispatch = useDispatch(); //to use the actions functions from slice use dispatch
+  //after creating a new note, navigate to /
+  const navigate = useNavigate();
+  //db.json gets updated when a note is created
+  const submit = async (formValues) => {
+    //alert(JSON.stringify(formValues));
+    const createdNote = await NoteAPI.create({
+      ...formValues,
+      created_at: new Date().toLocaleDateString(),
+    }); // + date of creation
+    dispatch(addNote(createdNote));
+    // alert(
+    //   "You have successfully submitted a new note!!",
+    //   JSON.stringify(formValues)
+    // );
+    navigate("/");
+  };
   return (
     <>
-      <NoteForm title="New note" />
+      <NoteForm title="New note" onSubmit={submit} />
     </>
   );
 }
