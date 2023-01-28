9c5c6681dce1040181dced2de6da355e1590c806
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-01-28 17:50:53 +0530

    changed build cmd

--- src/api/note-api.js
+++ src/api/note-api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://react-redux-note-app-manager.vercel.app/notes";
+const BASE_URL = "http://localhost:3200/notes";
 
 export class NoteAPI {
   static async create(formValues) {
