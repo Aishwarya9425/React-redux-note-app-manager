2fb528f5ea240346b8ee011627f1f40812dde9f4
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-02-11 21:20:21 +0530

    Update note-api.js

--- src/api/note-api.js
+++ src/api/note-api.js
@@ -9,6 +9,7 @@ export class NoteAPI {
 
   static async fetchAll() {
     //arr
+    console.log((await axios.get(`${BASE_URL}`)).data)
     return (await axios.get(`${BASE_URL}`)).data.map(this.formatId);
   }
 
